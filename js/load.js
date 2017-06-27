34b4fb26ff0e9a1faaec013f5d5b89695289f0ce
Maynard Lopez <nod32lopez@gmail.com>
2017-06-28 00:21:27 +0800

    - Updated art assets - Removed wav audio files - Removed start button in main menu, replaced by touching anywhere to start - Added main menu spaceman that shoots when main menu is tapped

--- js/load.js
+++ js/load.js
@@ -2,7 +2,10 @@ var loadState = {
 
     preload : function() {
         game.load.image('title', 'assets/art/title.png');
-        game.load.image('btn_start', 'assets/art/btn_start.png');
+        game.load.image('title_spaceman', 'assets/art/title_spaceman.png');
+        game.load.image('title_lazer', 'assets/art/title_lazer.png');
+        game.load.image('start', 'assets/art/start.png');
+        game.load.image('black', 'assets/art/black.png');
 
         game.load.image('spaceman', 'assets/art/spaceman.png');
         game.load.image('lazer', 'assets/art/lazer.png');
@@ -11,7 +14,6 @@ var loadState = {
         game.load.image('mover', 'assets/art/mover.png');
         game.load.image('shooter', 'assets/art/shooter.png');
         game.load.image('shooter_bullet', 'assets/art/shooter_bullet.png');
-        game.load.image('spawnvisual', 'assets/art/spawnvisual.png');
 
         game.load.image('screen_score', 'assets/art/screen_score.png');
         game.load.image('btn_retry', 'assets/art/btn_retry.png');
--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,58 @@
 var menuState = {
 
-    create : function() {
-        game.add.sprite(0, 100, 'title');
+    hasFired : false,
+    fireTime : null,
+    spaceman : null,
+    lazer : null,
 
-        var btnStart = game.add.sprite(SCREEN_CENTER.X, 650, 'btn_start');
-        btnStart.anchor.x = 0.5;
-        btnStart.anchor.y = 0.5;
-        btnStart.inputEnabled = true;
-        btnStart.events.onInputDown.add(this.goToPlayMode, this);
+    create : function() {
+        this.spaceman = game.add.sprite(150, SCREEN_CENTER.Y, 'title_spaceman');
+        this.spaceman.anchor.x = 0.5;
+        this.spaceman.anchor.y = 0.5;
+        game.physics.arcade.enable(this.spaceman);
+
+        this.lazer = game.add.sprite(0, 0, 'title_lazer');
+        this.lazer.anchor.x = -0.07;
+        this.lazer.anchor.y = 0.35;
+        this.lazer.visible = false;
+        this.spaceman.addChild(this.lazer);
+
+        var title = game.add.sprite(SCREEN_CENTER.X, 100, 'title');
+        title.anchor.x = 0.55;
+
+        var start = game.add.sprite(SCREEN_WIDTH - 64, SCREEN_HEIGHT - 64, 'start');
+        start.anchor.x = 1;
+        start.anchor.y = 1;
+
+        game.input.inputEnabled = true;
+        game.input.onTap.add(this.fire, this);
     },
 
     update : function() {
-        
+        if (this.hasFired) {
+            if (game.time.now >= this.fireTime) {
+                this.goToPlayMode();
+            }
+
+            var velocity = 1600;
+            var moveAngle = degToRad(this.spaceman.angle + 180);
+            this.spaceman.body.velocity.x = velocity * Math.cos(moveAngle);
+            this.spaceman.body.velocity.y = velocity * Math.sin(moveAngle);
+        }
+        else {
+            this.spaceman.angle += 0.05;
+        }
+    },
+
+    fire : function() {
+        if (this.hasFired) {
+            return;
+        }
+
+        this.fireTime = game.time.now + 2000;
+        this.hasFired = true;
+
+        this.lazer.visible = true;
     },
 
     goToPlayMode : function() {
